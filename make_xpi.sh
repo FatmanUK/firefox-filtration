52fd2874e93e7e6e6719a6c98bacf3a57653269b
Adam J. Richardson <fatman.uk@gmail.com>
2022-03-26 23:27:24 +0000

    Remove delete option from script

--- make_xpi.sh
+++ make_xpi.sh
@@ -8,10 +8,4 @@ mkdir -p pkgs
 echo Making xpi file.
 VER=$(/bin/jq <manifest.json .version | tr -d \")
 DATE=$(date +%Y%m%d%H%M%S)
-/bin/zip -r -FS -mx=9 fff-${VER}.${DATE}.xpi * --exclude '*.xpi' --exclude 'pkgs/*' --exclude '*.sh'
-
-#%ZIP% a -tzip -mx=9 %dest%.xpi chrome\*
-#%ZIP% a -tzip -mx=9 %dest%.xpi modules\*
-#%ZIP% a -tzip -mx=9 %dest%.xpi install.rdf
-#%ZIP% a -tzip -mx=9 %dest%.xpi chrome.manifest
-#%ZIP% a -tzip -mx=9 %dest%.xpi defaults\preferences\*
+/bin/zip -r -FS fff-${VER}.${DATE}.xpi * -x '*.xpi' -x 'pkgs/*' -x '*.sh'
